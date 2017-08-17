9fd99b5e4c1cf684b27ce093835b2312d62f2c97
CoreyFrench4 <Corey.french@hotmail.com>
2017-08-17 16:08:00 +1000

    Added billiards game and Colissions file

---
+++ Assets/6-Collisions/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collisions
+{
+    public class PlayerScript : MonoBehaviour
+    {
+        public float acceleration = 20f;
+        public float rotationSpeed = 360f;
+
+        private Rigidbody2D rigid;
+        
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Accelerate();
+            Rotate();
+        }
+
+        void Accelerate()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            rigid.AddForce(transform.up * inputV * acceleration);
+        }
+        void Rotate()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            //Vector3.back, angle
+            transform.Rotate(Vector3.back, rotationSpeed * inputH * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/6-Collisions/SpaceStation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Collisions
+{
+
+    public class SpaceStation : MonoBehaviour
+    {
+        
+        public float force = 30f;
+        private Rigidbody2D rigid;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            ContactPoint2D contact = other.contacts[0];
+            Vector3 direction = contact.normal;
+            rigid.AddForce(direction * force, ForceMode2D.Impulse);
+            
+        }
+      
+    }
+}
