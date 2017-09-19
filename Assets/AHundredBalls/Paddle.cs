f6ba5f716d3ee4e3355c13279d859c52c0cb3bbb
corey.french4 <corey.french@hotmail.com>
2017-09-19 12:10:31 +1000

    Animations

---
+++ Assets/AHundredBalls/Paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AHundredBalls
+{
+
+
+    public class Paddle : MonoBehaviour
+    {
+        private Animator anim;
+        public bool isOpened;
+        // Use this for initialization
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isOpened = true;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isOpened = false;
+            }
+        }
+        void UpdateAnimator()
+        {
+            anim.SetBool "isOpened", isOpened ;
+        }
+    }
+}
