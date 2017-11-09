9c0ddb5f78d8895d48be49e986dd1e5b9169f79f
corey.french4 <corey.french@hotmail.com>
2017-11-09 16:03:26 +1100

    Mannys huge package

---
+++ Assets/~MOBA/Scripts/CameraBounds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MOBA
+{
+    public class CameraBounds : MonoBehaviour
+    {
+        public Vector3 size = new Vector3(50f, 15f, 80f);
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(transform.position, size);
+
+        }
+        public Vector3 GetAdjustedPos(Vector3 incomingPos)
+        {
+            Vector3 pos = transform.position;
+            Vector3 halfSize = size * 0.5f;
+
+            // Is incomingPos.z outside positive Z?
+            if (incomingPos.z > pos.z + halfSize.z)
+                incomingPos.z = pos.z + halfSize.z;
+            
+            if (incomingPos.z < pos.z - halfSize.z)
+                incomingPos.z = pos.z - halfSize.z;
+
+            if (incomingPos.y > pos.y + halfSize.y)
+                incomingPos.y = pos.y + halfSize.y;
+
+            if (incomingPos.y < pos.y - halfSize.y)
+                incomingPos.y = pos.y - halfSize.y;
+
+            if (incomingPos.x > pos.x + halfSize.x)
+                incomingPos.x = pos.x + halfSize.x;
+
+            if (incomingPos.x < pos.x - halfSize.x)
+                incomingPos.x = pos.x - halfSize.x;
+            return incomingPos;
+        }
+       
+    }
+}
